ec7fc32bd2b1990fa32b31aad90afdb903bc3e0f
amoungui <samoungui@gmail.com>
2024-02-05 08:02:13 +0100

    add comment to the main.js file

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,84 +1,72 @@
-/******* Variables globaux *******/
-// recup du token dans le session storage
+// Récupération du token depuis le sessionStorage
 let token = window.sessionStorage.getItem("token");
 
-//Récupération des fiches eventuellement stockées dans le sessionStorage
+// Récupération des works depuis le sessionStorage
 let works = window.sessionStorage.getItem("works");
+
+// Récupération des éléments du DOM pour la galerie et les filtres
 const gallery = document.querySelector(".gallery");
 const filters = document.querySelector(".navigation");
 
-// deconnexion
+// Ajout d'un écouteur d'événement sur le bouton de déconnexion pour vider le sessionStorage
 const deconnect = document.getElementById("logout");
 deconnect.addEventListener("click", (event) => {
   window.sessionStorage.clear();
 });
-//cache filtres si login
+
+// Si un token est présent, on affiche les éléments d'édition et on cache le bouton de connexion
 if (token) {
   let jsEdition = document.querySelectorAll(".jsEdition");
   for (let i = 0; i < jsEdition.length; i++) {
     jsEdition[i].style.display = null;
   }
-  // document.getElementById("filtres").style.display = "none"
   document.getElementById("login").style.display = "none";
 }
 
-// Récupération des fiches depuis l'api
+// Récupération des works depuis l'API
 const askApi = await fetch("http://localhost:5678/api/works");
 works = await askApi.json();
 
-
-// Création des Fiches Projets
+// Fonction pour générer les works dans la galerie
 async function genererWorks(works) {
-
-  // Récupération de l'élément du DOM qui accueille les fiches
+  // Récupération de l'élément du DOM qui accueillera les works
   const sectionGallery = document.querySelector(".gallery");
   sectionGallery.innerHTML = ""
 
+  // Pour chaque work, on crée une balise <figure> avec une image et un titre
   works.forEach((work) => {
-    //création de la balise pour les fiches - balise<figure>
     const ficheElement = document.createElement("figure");
     ficheElement.classList.add("figureGallery")
     ficheElement.dataset.index = works.id
 
-    //Création des images
     const imageElement = document.createElement("img");
     imageElement.src = work.imageUrl;
     const titleElement = document.createElement("figcaption");
     titleElement.innerText = work.title;
 
-    // On rattache la balise <article> a la <div gallery>
     sectionGallery.appendChild(ficheElement);
-
-    //Rattachement de des balises au DOM
     ficheElement.appendChild(imageElement);
     ficheElement.appendChild(titleElement);
   })
 }
 
-//Création des fiches
+// Génération des works
 await genererWorks(works);
 
-//Filtres
-//***************Affichage des boutons par catégories*************/
-
-//récupérer le tableau des catégories
+// Fonction pour récupérer les catégories depuis l'API
 async function getCategories() {
   const response = await fetch("http://localhost:5678/api/categories");
   return await response.json();
 }
 
-getCategories()
-
-// function qui prend une chaine et la met la première lettre
-// en majuscule
+// Fonction pour mettre la première lettre d'une chaîne en majuscule
 function capitalize(myString) {
-  const capitalizedString = myString.charAt(0).toUpperCase() + myString.slice(1);
-  return capitalizedString;
+  return myString.charAt(0).toUpperCase() + myString.slice(1);
 }
 
+// Fonction pour afficher les liens de catégories
 async function displayCategoriesLinks() {
   const categories = await getCategories();
-  console.log(categories);
   categories.forEach((category) => {
     const btn = document.createElement("a");
     btn.textContent = capitalize(category.name);
@@ -87,41 +75,37 @@ async function displayCategoriesLinks() {
     btn.classList.add("nav_link", category.name.replace(/\s+/g, '-')); // Remplace les espaces par des tirets
     
     filters.appendChild(btn);
-});
+  });
 }
 
 await displayCategoriesLinks();
 
-// filtrer au click sur le bouton par catégorie
-
+// Fonction pour filtrer les works par catégorie
 async function filterByCategory() {
   const links = document.querySelectorAll(".navigation a");
-  console.log('liste des liens: ', links)
   links.forEach((link) => {
-      link.addEventListener("click", (e) => {
-          e.preventDefault();
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
 
-          // Supprimer la classe active de tous les liens
-          links.forEach((link) => {
-            link.classList.remove("active");
-          });
-
-          // Ajouter la classe active au lien cliqué
-          e.target.classList.add("active");
-
-          const linkId = e.target.id;
-          gallery.innerHTML = "";
-          if (linkId !== "0") {
-              const worksFilterByCategory = works.filter((work) => {
-              return work.categoryId == linkId;
-              });
-              genererWorks(worksFilterByCategory);
-              
-          } else {
-            genererWorks(works)
-          }
-          console.log(linkId);
+      // Suppression de la classe active de tous les liens
+      links.forEach((link) => {
+        link.classList.remove("active");
       });
+
+      // Ajout de la classe active au lien cliqué
+      e.target.classList.add("active");
+
+      const linkId = e.target.id;
+      gallery.innerHTML = "";
+      if (linkId !== "0") {
+        const worksFilterByCategory = works.filter((work) => {
+          return work.categoryId == linkId;
+        });
+        genererWorks(worksFilterByCategory);
+      } else {
+        genererWorks(works)
+      }
+    });
   });
 }
 
