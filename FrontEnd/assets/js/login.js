667c5967f066510197ae7ffabb0a0177c0ed40fd
amoungui <samoungui@gmail.com>
2024-02-06 15:44:12 +0100

    Notification management due to errors in filling in fields in the login form

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -3,6 +3,7 @@ const loginForm = document.getElementById("loginForm");
 const connect = document.getElementById("connect");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const notification = document.getElementById("notification");
 
 // Ajout d'un écouteur d'événement sur le bouton de connexion pour sélectionner le champ de mot de passe
 connect.addEventListener("click", (event) => {
@@ -14,30 +15,36 @@ loginForm.addEventListener("submit", async (event) => {
   // Prévention du comportement par défaut du formulaire
   event.preventDefault();
 
-  // Création d'un objet JSON avec les valeurs des champs email et password du formulaire
-  const bodyJson = JSON.stringify({
-    email: email.value,
-    password: password.value,
-  });
+  // Vérification si les champs email et password ne sont pas vides
+  if (email.value !== "" && password.value !== ""){
+    // Création d'un objet JSON avec les valeurs des champs email et password du formulaire
+    const bodyJson = JSON.stringify({
+      email: email.value,
+      password: password.value,
+    });
+    console.log(bodyJson);
+    // Envoi de la requête de connexion à l'API
+    const login = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: bodyJson,
+    });
 
-  // Envoi de la requête de connexion à l'API
-  const login = await fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: bodyJson,
-  });
+    // Récupération de la réponse de l'API
+    const data = await login.json();
 
-  // Récupération de la réponse de l'API
-  const data = await login.json();
-
-  // Si la réponse ne contient pas de token, on affiche une alerte avec le statut de la réponse
-  if (data.token == null) {
-    alert("User " + login.statusText);
-  } else {
-    // Sinon, on stocke le token dans le sessionStorage et on redirige l'utilisateur vers la page index.html
-    window.sessionStorage.setItem("token", data.token);
-    window.location.href = "./index.html";
+    // Si la réponse ne contient pas de token, on affiche une alerte avec le statut de la réponse
+    if (data.token == null) {
+      alert("This user is  "+login.statusText)
+    } else {
+      // Sinon, on stocke le token dans le sessionStorage et on redirige l'utilisateur vers la page index.html
+      window.sessionStorage.setItem("token", data.token);
+      window.location.href = "./index.html";
+    }
+  }else{
+    notification.innerHTML = "Un des champs est vide ou a mal été renseigné!";
+    notification.style.display = null;
   }
 });
